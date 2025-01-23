37086c60b41db733c14ac2ef9894be2b69ce50b9
Aditya201514049 <adityasingha084@gmail.com>
2025-01-23 20:34:06 +0600

    mongodb atlas connected

--- backend/config/config.js
+++ backend/config/config.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 module.exports = {
     PORT: process.env.PORT || 5000,
-    MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/task-manager',
+    MONGO_URI: process.env.MONGO_URI || 'mongodb+srv://adityasingha084:TXGppscjJiH8RbFp@cluster0.kmt8u.mongodb.net/Task-manager?retryWrites=true&w=majority&appName=Cluster0',
     JWT_SECRET: process.env.JWT_SECRET ,
     JWT_EXPIRATION: process.env.JWT_EXPIRATION || '1h',  // Default expiration for JWT
 };
