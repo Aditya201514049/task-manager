7e28515cb87d74c744ec9b65049a73d92b891a89
Aditya201514049 <adityasingha084@gmail.com>
2025-04-25 23:27:04 +0600

    Update JWT token generation in userController to remove expiration time, simplifying token management.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -52,7 +52,7 @@ const loginUser = async (req, res) => {
         }
 
         // Generate JWT token
-        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
 
         res.json({ token });
     } catch (error) {
